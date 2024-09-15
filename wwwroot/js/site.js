85ec580cd6d2da6ba12046eef11e6e76d398c26f
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-09-14 19:51:38 -0500

    feat: Toggle tab

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,15 +1,32 @@
 ﻿const btnSelect = document.querySelectorAll('.select');
 const selectDates = document.getElementById('selectDates');
 const sites = document.getElementById('sites');
+const main = document.querySelector('.main-content');
+
+const sitesTable = document.getElementById('sitesTable');
+const siteDetail = document.getElementById('siteDetail');
 
 btnSelect.forEach(button => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (event) => {
         selectDates.classList = 'main-title';
         sites.parentNode.className = 'text-black';
+
+        const btn = event.target;
+
+        const nombre = btn.getAttribute('data-name');
+        const descripcion = btn.getAttribute('data-description');
+        const tipo = btn.getAttribute('data-type');
+        const ubicacion = btn.getAttribute('data-ubication');
+
+        sitesTable.style.display = 'none';
+        siteDetail.style.display = 'block';
     });
 });
 
 sites.addEventListener("click", () => {
     selectDates.className = 'text-black-50';
     sites.parentNode.className = 'main-title';
+
+    sitesTable.style.display = 'table';
+    siteDetail.style.display = 'none';
 })
